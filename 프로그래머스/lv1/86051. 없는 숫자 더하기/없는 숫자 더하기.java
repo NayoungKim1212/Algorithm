089e4bfc9fcd4e31337a5048774ea63310388a10
NayoungKim1212 <132897437+NayoungKim1212@users.noreply.github.com>
2023-06-17 11:02:44 +0900

    Update 없는 숫자 더하기.java

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,9 +2,9 @@ class Solution {
      public static int solution(int[] numbers) {
         int answer = 0;
         
-        for (int i = 0; i < numbers.length; i++) {
-            answer += numbers[i];
+        for (int i = 0; i < numbers.length; i++) { // 0부터 number의 길이만큼 for문을 사용해 반복 출력
+            answer += numbers[i]; // 출력해서 나온 값을 다 더한 후 answer에 저장
         }
-        return 45 -answer;
+        return 45 -answer; // 
     }
 }
