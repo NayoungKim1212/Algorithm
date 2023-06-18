ae47a3d6b9f4cc77f9fa0dc296852dcc2ae607cd
NayoungKim1212 <132897437+NayoungKim1212@users.noreply.github.com>
2023-06-18 17:53:02 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -3,32 +3,19 @@ import java.time.LocalDate;
 class Solution {
     public String solution(int a, int b) {
             String answer = "";
-            LocalDate date = LocalDate.of(2016, a, b);
-            DayOfWeek dayOfWeek = date.getDayOfWeek();
-            int dayOfWeekNumber = dayOfWeek.getValue();
 
-            switch(dayOfWeekNumber){
-                case 1 :
-                    answer = "MON";
-                    break;
-                case 2 :
-                    answer = "TUE";
-                    break;
-                case 3 :
-                    answer = "WED";
-                    break;
-                case 4 :
-                    answer = "THU";
-                    break;
-                case 5 :
-                    answer = "FRI";
-                    break;
-                case 6:
-                    answer = "SAT";
-                    break;
-                case 7 :
-                    answer = "SUN";
+            int[] c = {31,29,31,30,31,30,31,31,30,31,30,31};
+            String[] MM ={"FRI","SAT","SUN","MON","TUE","WED","THU"};
+            int Adate = 0; // 2016년의 몇번째날인가?
+            // Adate를 구하기 위한 부분
+            for(int i = 0 ; i< a-1; i++){
+                Adate += c[i];
             }
+            Adate += b-1;
+
+            // 나누기 7을 했을 때 나머지로 요일 찾기
+            answer = MM[Adate %7];
+
             return answer;
     }
 }
