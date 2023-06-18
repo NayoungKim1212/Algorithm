1358a94a2d590ec97ec2d429dfd091620cdc5f56
NayoungKim1212 <132897437+NayoungKim1212@users.noreply.github.com>
2023-06-19 00:05:18 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean solution(String s) {
-               boolean answer = true;
-        if(s.length()!=4 && s.length()!=6) return false;
-
-        else {
-            for (int i = 0; i < s.length(); i++) {
-                int check = (int)(s.charAt(i));
-                if (!(check > 47 && check < 58)) { // 0~9의 아스키코드 값 -> 48~57
-                    return false;
-                }
+        boolean answer = true;
+        if (s.length() != 4 && s.length() != 6) return false;
+        for (int i = 0; i < s.length(); i++) {
+            int check = (int)(s.charAt(i));
+            if(s.charAt(i) < '0' || s.charAt(i) > '9') {
+                return false;
             }
         }
         return answer;
