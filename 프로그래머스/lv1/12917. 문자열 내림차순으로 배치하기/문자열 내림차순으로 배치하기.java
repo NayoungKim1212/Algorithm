b82535d0d97869f7434d637a138db709b0bf9ef0
NayoungKim1212 <132897437+NayoungKim1212@users.noreply.github.com>
2023-06-19 11:31:46 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 15.55 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,27 +1,27 @@
 import java.util.Arrays;
 class Solution {
-    public String solution(String s) {
-        // 소문자 배열과 대문자 문자열 나눠야해요
-        String up_s = "";
-        String down_s = "";
-        for(int i = 0; i < s.length(); i++){
-            if(Character.isUpperCase(s.charAt(i)))
-                up_s += s.charAt(i);
-            else down_s += s.charAt(i);
-        }
+        public String solution(String s) {
+            // 소문자 배열과 대문자 문자열 나눠야해요
+            String up_s = "";
+            String down_s = "";
+            for(int i = 0; i < s.length(); i++){
+                if(Character.isUpperCase(s.charAt(i)))
+                    up_s += s.charAt(i);
+                else down_s += s.charAt(i);
+            }
 
-        String[] UP = up_s.split("");
-        String[] DOWN = down_s.split("");
+            String[] UP = up_s.split("");
+            String[] DOWN = down_s.split("");
 
-        Arrays.sort(UP);
-        Arrays.sort(DOWN);
+            Arrays.sort(UP);
+            Arrays.sort(DOWN);
 
-        String answer = "";
-        // 소문자를 거꾸로 붙이기
-        for(int i = DOWN.length-1; i >= 0; i--)
-            answer += DOWN[i];
-        for(int i = UP.length-1; i >= 0; i--)
-            answer += UP[i];
-        return answer;
+            String answer = "";
+            // 소문자를 거꾸로 붙이기
+            for(int i = DOWN.length-1; i >= 0; i--)
+                answer += DOWN[i];
+            for(int i = UP.length-1; i >= 0; i--)
+                answer += UP[i];
+            return answer;
     }
 }
