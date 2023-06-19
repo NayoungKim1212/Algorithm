ffe487c16f4d1c87a406a76b6ecd4a8497d1068f
NayoungKim1212 <132897437+NayoungKim1212@users.noreply.github.com>
2023-06-19 17:59:15 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,21 +2,11 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 class Solution {
     public int solution(String s) {
-                HashMap<String, String> map = new LinkedHashMap<String, String>();
-        map.put("zero", "0"); map.put("one", "1");
-        map.put("two", "2"); map.put("three", "3");
-        map.put("four", "4"); map.put("five", "5");
-        map.put("six", "6"); map.put("seven", "7");
-        map.put("eight", "8"); map.put("nine", "9");
-
-
-        for(String key : map.keySet()){
-            if(s.contains(key)){
-                s = s.replace(key, map.get(key));
+         String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            for (int i = 0; i < 10; i++) {
+                s = s.replace(num[i], Integer.toString(i));
             }
-        }
-
-        int answer = Integer.parseInt(s);
-        return answer;
+            int answer = Integer.parseInt(s);
+            return answer;
     }
 }
