66b930cc562ee110180c9d5ad97832cc6391b967
NayoungKim1212 <132897437+NayoungKim1212@users.noreply.github.com>
2024-08-28 15:00:54 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(String[] strlist) {
         int[] answer = new int[strlist.length];
         
-        for(int i = 0; i < answer.length; i++) {
+        for(int i = 0; i < strlist.length; i++) {
             answer[i] = strlist[i].length();
         }
         return answer;
